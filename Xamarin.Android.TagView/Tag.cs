d9f2a3c589796b31662e8aee667f825c2044f152
Hackami <Hackami@users.noreply.github.com>
2017-12-23 09:23:27 +0330

    adding typeface

--- Xamarin.Android.TagView/Tag.cs
+++ Xamarin.Android.TagView/Tag.cs
@@ -1,4 +1,5 @@
 using System;
+using Android.Graphics;
 using Android.Graphics.Drawables;
 
 namespace com.cunoraz.tagview
@@ -20,6 +21,7 @@ namespace com.cunoraz.tagview
         public float layoutBorderSize;
         public int layoutBorderColor;
         public Drawable background;
+        public Typeface typeFace;
 
 
         public Tag(String text)
